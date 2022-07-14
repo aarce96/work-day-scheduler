2ea751ffcbb2a326fa74d3fd35c75f847a7f7050
Arturo Arce <arturo.arce@Arturos-MacBook-Pro.local>
2022-07-14 18:09:12 -0400

    add localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,29 @@ var currentDate = moment().format("MMMM Do YYYY");
 /* jQuery selector to grab the element with an ID, currentDay */
 $("#currentDay").text(currentDate);
 
+/* save task function */
+$(".saveBtn").on("click", function() {
+    alert("Task Saved!");
+
+    /* created an ID for each time block in div element */
+    var time = $(this)
+        .parent()
+        .attr("id");
+    /* each time block has a class of description */
+    var text = $(this)
+        .siblings(".description")
+        .val();
+
+    localStorage.setItem(time, text);
+});
+
+/* get local storage information */
+$("#9AM .description").val(localStorage.getItem("9AM"));
+$("#10AM .description").val(localStorage.getItem("10AM"));
+$("#11AM .description").val(localStorage.getItem("11AM"));
+$("#12PM .description").val(localStorage.getItem("12PM"));
+$("#1PM .description").val(localStorage.getItem("1PM"));
+$("#2PM .description").val(localStorage.getItem("2PM"));
+$("#3PM .description").val(localStorage.getItem("3PM"));
+$("#4PM .description").val(localStorage.getItem("4PM"));
+$("#5PM .description").val(localStorage.getItem("5PM"));
